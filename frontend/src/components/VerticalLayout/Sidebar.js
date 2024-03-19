532176cd041fe74f4f54aa24d0800abdd329c7b2
Eswar280 <146178295+Eswar280@users.noreply.github.com>
2024-03-19 16:55:41 +0530

    Update Sidebar.js

--- frontend/src/components/VerticalLayout/Sidebar.js
+++ frontend/src/components/VerticalLayout/Sidebar.js
@@ -1,58 +1,51 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { connect } from "react-redux";
-import { withRouter, Link } from "react-router-dom";
+import PropTypes from "prop-types"
+import React from "react"
+import { connect } from "react-redux"
+import { withRouter, Link } from "react-router-dom"
 
 //i18n
-import { withTranslation } from "react-i18next";
-import SidebarContent from "./SidebarContent";
+import { withTranslation } from "react-i18next"
+import SidebarContent from "./SidebarContent"
 
-import avatar2 from "../../assets/images/users/avatar-2.jpg";
+import avatar2 from "../../assets/images/users/avatar-2.jpg"
 
-const Sidebar = (props) => {
+const Sidebar = props => {
   return (
     <React.Fragment>
-      <div className="vertical-menu" style={{ height: "calc(100vh - 110px)" }}>
+      <div className="vertical-menu">
         <div className="h-100">
-          <div className="user-wid text-center py-3">
+          <div className="user-wid text-center py-4">
             <div className="user-img">
-              <img
-                src={avatar2}
-                alt=""
-                className="avatar-md mx-auto rounded-circle"
-              />
+              <img src={avatar2} alt="" className="avatar-md mx-auto rounded-circle" />
             </div>
 
-            <div className="mt-2">
-              <Link to="#" className="text-dark fw-medium font-size-16">
-                Patrick Becker
-              </Link>
+            <div className="mt-3">
+              <Link to="#" className="text-dark fw-medium font-size-16">Patrick Becker</Link>
               <p className="text-body mt-1 mb-0 font-size-13">UI/UX Designer</p>
             </div>
           </div>
-          <div data-simplebar className="h-100">
-            {props.type !== "condensed" ? (
+          <div className="h-100">
+            <div className="d-flex flex-column justify-content-between">
               <SidebarContent />
-            ) : (
-              <SidebarContent />
-            )}
+            </div>
           </div>
         </div>
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
 Sidebar.propTypes = {
   type: PropTypes.string,
-};
+}
 
-const mapStatetoProps = (state) => {
+const mapStatetoProps = state => {
   return {
     layout: state.Layout,
-  };
-};
+  }
+}
+
 export default connect(
   mapStatetoProps,
   {}
-)(withRouter(withTranslation()(Sidebar)));
+)(withRouter(withTranslation()(Sidebar)))
